e08c2c7b586274696f6fcca10e1c18d20684f429
Benjamin Wang <benjylxwang64@gmail.com>
2021-06-14 17:20:51 +0100

    Update pi-startup-script.sh

--- pi-startup-script.sh
+++ pi-startup-script.sh
@@ -3,4 +3,11 @@
 HOST=192.168.1.167
 KEY=test
 
+ffprobe -v quiet -print_format json -show_streams rtmp://$HOST/live/$KEY
+
+while [ $? -ne 0 ]
+do 
+    ffprobe -v quiet -print_format json -show_streams rtmp://$HOST/live/$KEY
+done
+
 vlc --fullscreen rtmp://$HOST/live/$KEY
